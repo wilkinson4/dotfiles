83fbf3300beb0a879c0f785255f65cd6fba0acaa
William Wilkinson <will.wilkinson@carebridgehealth.com>
2024-12-11 22:40:04 -0600

    Add missing harpoon config

--- dot_config/nvim/plugin/keys.lua
+++ dot_config/nvim/plugin/keys.lua
@@ -17,3 +17,39 @@ vim.cmd 'vmap <buffer> <expr> gs db#op_exec()'
 
 vim.api.nvim_set_keymap('n', '<leader>mp', '<cmd>Octo search author:wilkinson4 is:pr is:open<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>ra', '<cmd>Octo reviewer add<cr>', { noremap = true })
+
+-- Harpoon
+local harpoon = require("harpoon")
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end)
+
+-- Clear Harpoon list
+vim.keymap.set("n", "<leader>x", function() harpoon:list():clear() end)
